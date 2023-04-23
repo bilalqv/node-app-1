41d564c0c1ca1dd6387bbc5cc23ed0ee1c5c2c24
Bilal <crediblebilal@gmail.com>
2023-04-23 19:07:20 +0530

    added api for db syncing

--- routes/Person.js
+++ routes/Person.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const personRouter = require('../controller/Person');
 
-// router.get('/createTable', personRouter.createTable);
+router.get('/createTable', personRouter.createTable);
 router.get('/all', personRouter.allData);
 router.get('/single', personRouter.getSingle);
 router.post('/create', personRouter.createRow);
